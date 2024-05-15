a957eae315cb01488e44287e0f8a1f9d263c8d50
riteshnath <nathritesh98@gmail.com>
2024-05-15 22:00:55 +0530

    Complete investment calculator project

--- 03-Investment-Calculator/src/App.jsx
+++ 03-Investment-Calculator/src/App.jsx
@@ -3,17 +3,35 @@ import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput";
 import Results from "./components/Results";
 
-let INPUT = {};
-
 function App() {
-  const [results, setResults] = useState(false);
-  function getFormData(data) {
-    INPUT = data;
+  // State variables to store the input values
+  const [userInput, setuserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  // Function to update form data when any input field changes
+  function handleChange(inputIdentifier, newValue) {
+    setuserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
   }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
--- 03-Investment-Calculator/src/components/Results.jsx
+++ 03-Investment-Calculator/src/components/Results.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
-export default function Results({ userInputData }) {
-  let results = calculateInvestmentResults(userInputData);
+export default function Results({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+
   return (
     <table id="result">
       <thead>
@@ -15,14 +20,22 @@ export default function Results({ userInputData }) {
         </tr>
       </thead>
       <tbody>
-        {results.map((result, index) => {
-          <tr key={index}>
-            <td>{result.year}</td>
-            <td>{result.valueEndOfYear}</td>
-            <td>{result.interest}</td>
-            <td>{result.interest}</td>
-            <td>{result.annualInvestment}</td>
-          </tr>;
+        {resultsData.map((result, index) => {
+          const totalInterest =
+            result.valueEndOfYear -
+            result.annualInvestment * result.year -
+            initialInvestment;
+
+          const totalAmountInvested = result.valueEndOfYear - totalInterest;
+          return (
+            <tr key={index}>
+              <td>{result.year}</td>
+              <td>{formatter.format(result.valueEndOfYear)}</td>
+              <td>{formatter.format(result.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
         })}
       </tbody>
     </table>
