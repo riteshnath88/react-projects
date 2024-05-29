2457e5246a080c49df898ebe32237775e0ac3f29
riteshnath <nathritesh98@gmail.com>
2024-05-29 22:06:42 +0530

    Storing name

--- 05-Refs-Portals/src/components/Player.jsx
+++ 05-Refs-Portals/src/components/Player.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react-dom";
+import { useState } from "react";
 
 export default function Player() {
+  const [enteredPlayername, setEnteredPlayername] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayername(event.target.value);
+  }
+
+  function handleClick(event) {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayername : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayername} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
